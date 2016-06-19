52da900df7a3f93fdf6d3b65d2c49eb3230fa28f
p001984 <h.tammen@tammen-it-solutions.de>
2016-06-19 19:25:26 +0000

    Enhancement and I18N

---
+++ webapp/view/SharedBlocks/services/BlockServices.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+		"sap/uxap/BlockBase"
+	], function (BlockBase) {
+	"use strict";
+
+	var BlockServices = BlockBase.extend("de.tammenit.ui5.homepage.view.SharedBlocks.services.BlockServices", {
+		metadata: {}
+	});
+	return BlockServices;
+}, true);
