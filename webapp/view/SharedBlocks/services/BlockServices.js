0a4db68a0abfd51d871448eebcccb000c7047296
p001984 <h.tammen@tammen-it-solutions.de>
2016-06-19 20:51:39 +0000

    Services

--- webapp/view/SharedBlocks/services/BlockServices.js
+++ webapp/view/SharedBlocks/services/BlockServices.js
@@ -1,10 +1,15 @@
 sap.ui.define([
-		"sap/uxap/BlockBase"
-	], function (BlockBase) {
+		"sap/uxap/BlockBase",
+		"sap/m/LabelDesign"
+	], function (BlockBase, LabelDesign) {
 	"use strict";
 
 	var BlockServices = BlockBase.extend("de.tammenit.ui5.homepage.view.SharedBlocks.services.BlockServices", {
-		metadata: {}
+		metadata: {},
+		
+		serviceFormatter: function(rating) {
+			return (rating === 1 ? LabelDesign.Bold : LabelDesign.Standard);
+		}
 	});
 	return BlockServices;
 }, true);
