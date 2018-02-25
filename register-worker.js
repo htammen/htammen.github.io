b176ee4f0a5bd3ef57d45d974ee1f3b926622c06
Tammen Helmut <h.tammen@tammen-it-solutions.de>
2018-02-25 17:14:55 +0000

    PWA next

--- register-worker.js
+++ register-worker.js
@@ -2,6 +2,9 @@
  * Register the service worker
  */
 if ('serviceWorker' in navigator) {
+  // load serviceworker after page has been loaded. This ensures a best first-visit experience.
+  // see here for details: https://developers.google.com/web/fundamentals/primers/service-workers/registration
+  window.addEventListener('load', function() {
     navigator.serviceWorker
         .register('./service-worker.js')
         .then(function () { 
@@ -10,4 +13,5 @@ if ('serviceWorker' in navigator) {
             console.log('Service Worker Registration failed');
             console.log(error);
         });
+  });
 }
