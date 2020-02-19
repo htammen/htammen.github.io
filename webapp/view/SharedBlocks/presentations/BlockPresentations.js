60aa7d8fd7264a269adc93a07b848e83f04b4fd1
Helmut Tammen <h.tammen@tammen-it-solutions.de>
2020-02-19 17:46:23 +0100

    Added Presentation section, added some pictures

---
+++ webapp/view/SharedBlocks/presentations/BlockPresentations.js
@@ -0,0 +1,12 @@
+sap.ui.define(["sap/uxap/BlockBase"], function(BlockBase) {
+  "use strict";
+
+  var BlockPresentations = BlockBase.extend(
+    "de.tammenit.ui5.homepage.view.SharedBlocks.presentations.BlockPresentations",
+    {
+      metadata: {}
+    }
+  );
+
+  return BlockPresentations;
+});
