78dad0c6a9a8035c63bdeaf31f98068714e08e0d
Helmut Tammen <h.tammen@tammen-it-solutions.de>
2021-10-13 19:44:44 +0200

    deletion of files in /dist folder

--- dist/controller/SharedBlocks/itskills/ITSkill-dbg.controller.js
+++
@@ -1,9 +0,0 @@
-sap.ui.define([
-	"de/tammenit/ui5/homepage/controller/SharedBlocks/BaseBlockController.controller"
-], function(BaseController) {
-	"use strict";
-
-	return BaseController.extend("de.tammenit.ui5.homepage.controller.SharedBlocks.itskills.ITSkill", {
-	});
-
-});
